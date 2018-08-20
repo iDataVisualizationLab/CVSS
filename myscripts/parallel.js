4898a9e9c0f607c4a4555a1ea8a2591168cfa0eb
Tommy Dang <nhontuan@gmail.com>
2018-08-19 19:26:01 -0500

    Update TD

--- myscripts/parallel.js
+++ myscripts/parallel.js
@@ -99,17 +99,11 @@ var svg = d3.select("#parallelSVG")
 // Load the data and visualization
 // d3.json("data/nvdcve-1.0-2014.json", function (raw_data) {      // 2014 -> 653 CVEs
 //d3.json("data/nvdcve-1.0-2016.json", function(raw_data) {
-<<<<<<< HEAD
-//d3.json("data/nvdcve-1.0-2017.json", function(raw_data) {  // 2017 -> 12,829 CVEs
-d3.json("data/nvdcve-1.0-2018.json", function(raw_data) {
-
-=======
 // d3.json("data/nvdcve-1.0-2017.json", function(raw_data) {  // 2017 -> 12,829 CVEs
 // d3.json("data/nvdcve-1.0-2018.json?raw=true", function(error, raw_data) {
 d3.json("data/isp1.json", function (error, raw_data) {
 // d3.json("data/nvdcve20172018.json", function (error, raw_data) {
 // d3.json("data/allCVEs.json", function (error, raw_data) {
->>>>>>> b69e74fdc000c46196bb8b17c4d2c682a57613d2
     // var data21 = raw_data.CVE_Items.filter(function(d) {return d.cve.problemtype.problemtype_data[0].description.length>1;})
     // var data22 = raw_data.CVE_Items.filter(function(d) {return d.cve.problemtype.problemtype_data.length>1;})
     //    var data33 = raw_data.CVE_Items.filter(function(d) {return d.cve.affects.vendor.vendor_data.length>2;})
